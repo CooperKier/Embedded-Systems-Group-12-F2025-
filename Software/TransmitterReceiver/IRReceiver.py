0a7dc744e9d8481ec46e49c5fb731b7ea2d8072e
Ashwin Fernandez <127068085+AshwinFernandez@users.noreply.github.com>
2025-10-16 11:45:09 -0600

    Fix syntax error in ir_callback function

--- Software/TransmitterReceiver/IRReceiver.py
+++ Software/TransmitterReceiver/IRReceiver.py
@@ -7,9 +7,9 @@ from ir_rx.print_error import print_error
 def ir_callback(data, addr, _):
   print(f"Received NEC Command! Data: 0x{data:02x} , Addr: 0x{addr:02x}" }
 
-  ir_pin = Pin(17, Pin.IN, Pin.PULL_UP)
-  ir_receiver = NEC_8(ir_pin, callback=ir_callback)
-  ir_receiver.error_function(print_error)
+ir_pin = Pin(17, Pin.IN, Pin.PULL_UP)
+ir_receiver = NEC_8(ir_pin, callback=ir_callback)
+ir_receiver.error_function(print_error)
 
 while (1):
   pass
