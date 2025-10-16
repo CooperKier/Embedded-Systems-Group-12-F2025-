aceb8289a3f23eadd6106ef4a200acc87e96eea7
Ashwin Fernandez <127068085+AshwinFernandez@users.noreply.github.com>
2025-10-16 11:43:14 -0600

    Add IRReceiver.py for NEC IR command handling

---
+++ Software/TransmitterReceiver/IRReceiver.py
@@ -0,0 +1,15 @@
+import ir_rx
+import machine
+from machine import Pin
+from ir_rx.nec import NEX_8
+from ir_rx.print_error import print_error
+
+def ir_callback(data, addr, _):
+  print(f"Received NEC Command! Data: 0x{data:02x} , Addr: 0x{addr:02x}" }
+
+  ir_pin = Pin(17, Pin.IN, Pin.PULL_UP)
+  ir_receiver = NEC_8(ir_pin, callback=ir_callback)
+  ir_receiver.error_function(print_error)
+
+while (1):
+  pass
