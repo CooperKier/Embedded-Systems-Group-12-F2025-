f43c14fb39340dae7ba263f915e3b7d004cea9b3
CooperKier <coki1339@colorado.edu>
2025-12-08 17:55:04 -0700

    Update sensors.py

--- Software/sensors.py
+++ Software/sensors.py
@@ -1,30 +1,15 @@
-from machine import Pin
-import time
+from machine import ADC, Pin
+from time import sleep
 
-# Interupts
-def sensor_N_irq(pin):
-    print(">>> Move motors in direction N")
-
-def sensor_W_irq(pin):
-    print(">>> Move motors in direction W")
-
-def sensor_S_irq(pin):
-    print(">>> Move motors in direction S")
-
-def sensor_E_irq(pin):
-    print(">>> Move motors in direction E")
-
-sensor_N = Pin(16, Pin.IN, Pin.PULL_DOWN)
-sensor_W = Pin(17, Pin.IN, Pin.PULL_DOWN)
-sensor_S = Pin(18, Pin.IN, Pin.PULL_DOWN)
-sensor_E = Pin(19, Pin.IN, Pin.PULL_DOWN)
-
-sensor_N.irq(trigger=Pin.IRQ_RISING, handler=sensor_N_irq)
-sensor_W.irq(trigger=Pin.IRQ_RISING, handler=sensor_W_irq)
-sensor_S.irq(trigger=Pin.IRQ_RISING, handler=sensor_S_irq)
-sensor_E.irq(trigger=Pin.IRQ_RISING, handler=sensor_E_irq)
+sensor = ADC(Pin(28))
+threshold = 30000   # Tune depending on your surface
 
 while True:
-    print("Main going ...")
-    time.sleep(1)
-
+    value = sensor.read_u16()
+    print(value)
+    if value > threshold:
+        print("Black detected")
+    else:
+        print("White detected")
+    
+    sleep(0.05)
